ad9a157fb026b3eb42f7f2d9e5c58215a4b7cd9c
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-31 21:08:36 +0300

    changed views to rest framwework built-ins

--- virtual_classroom/virtual_classroom/authentication/views.py
+++ virtual_classroom/virtual_classroom/authentication/views.py
@@ -1,44 +1,27 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
+from virtual_classroom.authentication.serializers import RegisterUserSerializer
+from rest_framework.authtoken import views as api_auth_views
+from rest_framework import generics as api_views
 from rest_framework.response import Response
-from virtual_classroom.authentication.serializers import UserSerializer
-from django.contrib.auth.models import User
 
 
-class RegisterView(APIView):
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            user.set_password(request.data['password'])
-            user.save()
-            
-            token = Token.objects.create(user=user)
+class ApiLoginUserView(api_auth_views.ObtainAuthToken):
+    pass
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginView(APIView):
-    def post(self, request, format=None):
-        username = request.data.get('username')
-        password = request.data.get('password')
+class ApiRegisterUserView(api_views.CreateAPIView):
+    serializer_class = RegisterUserSerializer
 
-        user = get_object_or_404(User, username=username)
 
-        if not user.check_password(password):
-            return Response({"detail": "Not found."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        token, created = Token.objects.get_or_create(user=user)
-        serializer = UserSerializer(instance=user)
-        context = {
-            'username': username
-        }
+class ApiLogoutUserView(api_views.views.APIView):
+    def post(self, request, *args, **kwargs):
+        return self.__perform_logout(request)
 
-        return render(request, 'auth/index.html', context=context)
+    def get(self, request, *args, **kwargs):
+        return self.__perform_logout(request)
 
-def login_page(request):
-    return render(request, 'auth/login.html')
-
-def register_page(request):
-    return render(request, 'auth/register.html')
+    @staticmethod
+    def __perform_logout(request):
+        request.user.auth_token.delete()
+        return Response({
+            'message': 'user logged out'
+        })
