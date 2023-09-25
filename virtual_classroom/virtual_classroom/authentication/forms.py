1a1830867b995d216d9d89fae508d2afe5881e4d
Alex <alex.lemuel@gmail.com>
2023-09-25 14:23:56 +0200

    set attributes to input fileds

--- virtual_classroom/virtual_classroom/authentication/forms.py
+++ virtual_classroom/virtual_classroom/authentication/forms.py
@@ -4,6 +4,21 @@ from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
 class UserRegistrationForm(auth_forms.UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "login__input"
+            field.widget.attrs["id"] = field_name
+
+            if field_name == "username":
+                field.widget.attrs["placeholder"] = "Username"
+            elif field_name == "email":
+                field.widget.attrs["placeholder"] = "Email"
+            elif field_name == "password1":
+                field.widget.attrs["placeholder"] = "Password"
+            elif field_name == "password2":
+                field.widget.attrs["placeholder"] = "Confirm Password"
+
     class Meta:
         model = UserModel
         fields = ('username', 'email', 'password1', 'password2')
